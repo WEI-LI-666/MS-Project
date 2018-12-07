17e6270d7a952bf65a5cb036f9d2bc064ef13de4
PowerChen <zxc851023@gmail.com>
2018-12-07 10:52:00 +0800

    files update

--- index.php
+++ index.php
@@ -7,12 +7,14 @@
     <link rel="stylesheet" type="text/css" href="pdb.css"/>
     <script src="https://d3js.org/d3.v3.min.js"></script>
     <script src="https://unpkg.com/topojson@3"></script>
-    <script type="text/javascript" src="index.js"></script>
+    <script type="text/javascript" src="js/index.js"></script>
     <script type="text/javascript" src="d3/d3.js"></script>
     <script type="text/javascript" src="plotdb.js"></script>
     <script type="application/json" src="ms_population.json"></script>
     
     <!--<iframe src="https://plotdb.io/v/chart/19614" width="100%" height="100%" allowfullscreen="true" frameborder="1"></iframe>-->
-
+    <body>
+    
+    </body>
     
 </html> 
